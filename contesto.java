235451e07c5cfc9f7a7dc39c3807a8906f98d6c7
LICENCE AAS <118910827+licencedaas@users.noreply.github.com>
2024-02-19 15:58:11 -0500

    Add files via upload

---
+++ contesto.java
@@ -0,0 +1,83 @@
+package licence.daas.ubicacion;
+
+public class contesto {
+
+
+
+    private  static String  keyLatitud, keyLogitud, keyMarca, keyUbicacionZonas;
+    private String keynombre, keyapellido, keyCelular, keyGmail, keyPass;
+
+
+
+    public String getKeyLatitud() {
+        return keyLatitud;
+    }
+
+    public void setKeyLatitud(String keyLatitud) {
+        this.keyLatitud = keyLatitud;
+    }
+
+    public String getKeyLogitud() {
+        return keyLogitud;
+    }
+
+    public void setKeyLogitud(String keyLogitud) {
+        this.keyLogitud = keyLogitud;
+    }
+
+    public String getKeyMarca() {
+        return keyMarca;
+    }
+
+    public void setKeyMarca(String keyMarca) {
+        this.keyMarca = keyMarca;
+    }
+
+    public String getKeyUbicacionZonas() {
+        return keyUbicacionZonas;
+    }
+
+    public void setKeyUbicacionZonas(String keyUbicacionZonas) {
+        this.keyUbicacionZonas = keyUbicacionZonas;
+    }
+
+    public String getKeynombre() {
+        return keynombre;
+    }
+
+    public void setKeynombre(String keynombre) {
+        this.keynombre = keynombre;
+    }
+
+    public String getKeyapellido() {
+        return keyapellido;
+    }
+
+    public void setKeyapellido(String keyapellido) {
+        this.keyapellido = keyapellido;
+    }
+
+    public String getKeyCelular() {
+        return keyCelular;
+    }
+
+    public void setKeyCelular(String keyCelular) {
+        this.keyCelular = keyCelular;
+    }
+
+    public String getKeyGmail() {
+        return keyGmail;
+    }
+
+    public void setKeyGmail(String keyGmail) {
+        this.keyGmail = keyGmail;
+    }
+
+    public String getKeyPass() {
+        return keyPass;
+    }
+
+    public void setKeyPass(String keyPass) {
+        this.keyPass = keyPass;
+    }
+}
